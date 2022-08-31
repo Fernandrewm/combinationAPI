7fcd96301c07537ca5bc4318d8110852e4eabbbc
Fernando Muñoz <fernandrewm@gmail.com>
2022-08-30 23:51:26 -0600

    logic apis conversion

--- serverless.ts
+++ serverless.ts
@@ -9,6 +9,8 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    profile: 'serverless_user',
+    region: 'us-west-2',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
